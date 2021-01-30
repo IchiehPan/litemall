55b00ba5f3935d381b3ed85521f4ffcf07ab7ec2
ichieh <ichieh@foxmail.com>
2021-01-30 09:28:27 +0800

    增加启动项目时打印项目所在的描述文档url地址注释

--- litemall-all/src/main/java/org/linlinjava/litemall/Application.java
+++ litemall-all/src/main/java/org/linlinjava/litemall/Application.java
@@ -1,19 +1,38 @@
 package org.linlinjava.litemall;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.linlinjava.litemall.admin.service.AdminGoodsService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.net.InetAddress;
 
 @SpringBootApplication(scanBasePackages = {"org.linlinjava.litemall"})
 @MapperScan("org.linlinjava.litemall.db.dao")
 @EnableTransactionManagement
 @EnableScheduling
 public class Application {
+    private static final Log logger = LogFactory.getLog(Application.class);
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext application = SpringApplication.run(Application.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        logger.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + "/swagger-ui.html\n\t" +
+                "Doc: \t\thttp://" + ip + ":" + port + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
